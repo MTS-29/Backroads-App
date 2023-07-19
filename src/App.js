80a85765a7bfd1c79dc4c3c7cb08fafe9e110a4a
MTS-29 <mtaquis@gmail.com>
2023-07-19 12:04:09 +0530

    Project structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
+import Footer from "./components/Footer";
 function App() {
-  return <h1>Backroads application</h1>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
